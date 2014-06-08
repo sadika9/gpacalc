4b0d31abc58a1860697e28c3e27345249928f50b
Sadika Sumanapala <sadikahs@gmail.com>
2014-06-08 19:26:37 +0530

    remove quotation marks around property names

--- data/grades.js
+++ data/grades.js
@@ -1,50 +1,50 @@
 var grades = [
   {
-    "grade": "A+",
-    "grade_point": 4
+    grade: "A+",
+    grade_point: 4
   },
   {
-    "grade": "A",
-    "grade_point": 4
+    grade: "A",
+    grade_point: 4
   },
   {
-    "grade": "A-",
-    "grade_point": 3.7
+    grade: "A-",
+    grade_point: 3.7
   },
   {
-    "grade": "B+",
-    "grade_point": 3.3
+    grade: "B+",
+    grade_point: 3.3
   },
   {
-    "grade": "B",
-    "grade_point": 3
+    grade: "B",
+    grade_point: 3
   },
   {
-    "grade": "B-",
-    "grade_point": 2.7
+    grade: "B-",
+    grade_point: 2.7
   },
   {
-    "grade": "C+",
-    "grade_point": 2.3
+    grade: "C+",
+    grade_point: 2.3
   },
   {
-    "grade": "C",
-    "grade_point": 2
+    grade: "C",
+    grade_point: 2
   },
   {
-    "grade": "C-",
-    "grade_point": 1.7
+    grade: "C-",
+    grade_point: 1.7
   },
   {
-    "grade": "D+",
-    "grade_point": 1.3
+    grade: "D+",
+    grade_point: 1.3
   },
   {
-    "grade": "D",
-    "grade_point": 1
+    grade: "D",
+    grade_point: 1
   },
   {
-    "grade": "F",
-    "grade_point": 0
+    grade: "F",
+    grade_point: 0
   }
 ];
--- data/subjects.js
+++ data/subjects.js
@@ -1,54 +1,54 @@
 var subjects = [
   {
-    "subject": "Biology",
-    "code": "BL"
+    subject: "Biology",
+    code: "BL"
   },
   {
-    "subject": "Botany",
-    "code": "BT"
+    subject: "Botany",
+    code: "BT"
   },
   {
-    "subject": "Chemistry",
-    "code": "CH"
+    subject: "Chemistry",
+    code: "CH"
   },
   {
-    "subject": "Computer Science",
-    "code": "CS"
+    subject: "Computer Science",
+    code: "CS"
   },
   {
-    "subject": "Geology",
-    "code": "GL"
+    subject: "Geology",
+    code: "GL"
   },
   {
-    "subject": "Mathematics",
-    "code": "MT"
+    subject: "Mathematics",
+    code: "MT"
   },
   {
-    "subject": "Molecular Biology and Biotechnology",
-    "code": "MB"
+    subject: "Molecular Biology and Biotechnology",
+    code: "MB"
   },
   {
-    "subject": "Physics",
-    "code": "PH"
+    subject: "Physics",
+    code: "PH"
   },
   {
-    "subject": "Statistics",
-    "code": "ST"
+    subject: "Statistics",
+    code: "ST"
   },
   {
-    "subject": "Zoology",
-    "code": "ZL"
+    subject: "Zoology",
+    code: "ZL"
   },
   {
-    "subject": "Economics",
-    "code": "EC"
+    subject: "Economics",
+    code: "EC"
   },
   {
-    "subject": "Management Studies",
-    "code": "MG"
+    subject: "Management Studies",
+    code: "MG"
   },
   {
-    "subject": "Science Education",
-    "code": "SE"
+    subject: "Science Education",
+    code: "SE"
   }
 ];
